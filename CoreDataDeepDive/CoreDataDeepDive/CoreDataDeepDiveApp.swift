d6700fb6c6cd7a9dd221872b087788cfb53af632
Abdullah Ansari <abdullahansari018@gmail.com>
2025-11-18 00:19:27 +0530

    Initial commit.

---
+++ CoreDataDeepDive/CoreDataDeepDive/CoreDataDeepDiveApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoreDataDeepDiveApp.swift
+//  CoreDataDeepDive
+//
+//  Created by Abdullah Ansari on 18/11/25.
+//
+
+import SwiftUI
+
+@main
+struct CoreDataDeepDiveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
