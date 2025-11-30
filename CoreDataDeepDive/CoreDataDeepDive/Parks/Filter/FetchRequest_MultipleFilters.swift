eb1e80a615b5c8575289ceef87beabc0c78f28f4
Abdullah Ansari <abdullahansari018@gmail.com>
2025-11-30 15:33:29 +0530

    Add FetchRequest_MultipleFilters

---
+++ CoreDataDeepDive/CoreDataDeepDive/Parks/Filter/FetchRequest_MultipleFilters.swift
@@ -0,0 +1,46 @@
+//
+//  FetchRequest_MultipleFilters.swift
+//  CoreDataDeepDive
+//
+//  Created by Abdullah Ansari on 30/11/25.
+//
+
+import SwiftUI
+
+struct FetchRequest_MultipleFilters: View {
+    
+    @FetchRequest<ParkEntity>(
+        sortDescriptors: [],
+        predicate: NSPredicate(format: "country == 'United States' AND region == 'Utah'")
+    ) private var parks
+    
+    var body: some View {
+        NavigationStack {
+            List(parks) { park in
+                HStack {
+                    Image(uiImage: park.viewImage)
+                        .resizable()
+                        .scaledToFit()
+                        .frame(height: 60)
+                        .cornerRadius(8)
+                    
+                    VStack(alignment: .leading, spacing: 4) {
+                        Text(park.viewName)
+                            .font(.title)
+                        Text(park.viewLocation)
+                            .fontWeight(.light)
+                    }
+                    
+                    Spacer()
+                }
+                .padding(.vertical, 8)
+            }
+            .navigationTitle("Parks")
+        }
+    }
+}
+
+#Preview {
+    FetchRequest_MultipleFilters()
+        .environment(\.managedObjectContext, ParksContainer(forPreview: true).persistentContainer.viewContext)
+}
