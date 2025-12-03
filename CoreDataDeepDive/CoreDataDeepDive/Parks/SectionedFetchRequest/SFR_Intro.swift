1b3229dd9691a832b901b29ce1b8e35bd7594005
Abdullah Ansari <abdullahansari018@gmail.com>
2025-12-03 23:30:36 +0530

    Add SFR_Intro

---
+++ CoreDataDeepDive/CoreDataDeepDive/Parks/SectionedFetchRequest/SFR_Intro.swift
@@ -0,0 +1,48 @@
+//
+//  SFR_Intro.swift
+//  CoreDataDeepDive
+//
+//  Created by Abdullah Ansari on 02/12/25.
+//
+
+import SwiftUI
+
+struct SFR_Intro: View {
+    @SectionedFetchRequest<String?, ParkEntity>(sectionIdentifier: \.country, sortDescriptors: [SortDescriptor(\.country)]) private var parks
+    
+    var body: some View {
+        NavigationStack {
+            List(parks) { section in
+                Section {
+                    ForEach(section) { park in
+                        HStack {
+                            Image(uiImage: park.viewImage)
+                                .resizable()
+                                .scaledToFit()
+                                .frame(height: 80)
+                                .cornerRadius(8)
+                            VStack(alignment: .leading, spacing: 4) {
+                                Text(park.viewName)
+                                    .font(.title2)
+                                Text(park.viewLocation)
+                                    .fontWeight(.light)
+                            }
+                        }
+                        .padding(.vertical, 8)
+                    }
+                } header: {
+                    Text(section.id!)
+                }
+            }
+            .headerProminence(.increased)
+            .listStyle(.insetGrouped)
+            .navigationTitle("Parks")
+            .navigationBarTitleDisplayMode(.large)
+        }
+    }
+}
+
+#Preview {
+    SFR_Intro()
+        .environment(\.managedObjectContext, ParksContainer(forPreview: true).persistentContainer.viewContext)
+}
